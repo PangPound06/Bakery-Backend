fe816f33093bba281fc3f9b6edd63641a8a46f1d
PangPound06 <pondpond6645@gmail.com>
2026-02-21 01:36:52 +0700

    Reset OTP

--- src/main/java/com/app/my_project/MyProjectApplication.java
+++ src/main/java/com/app/my_project/MyProjectApplication.java
@@ -3,25 +3,20 @@ package com.app.my_project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.context.annotation.ComponentScan;
-
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @RestController
 @ComponentScan(basePackages = "com.app.my_project")
-@EnableAsync
 public class MyProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyProjectApplication.class, args);
 	}
 
-	/*
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello, World!!!";
 	}
-	*/
 }
