b26dbf1859e3af0b0ca3c9efcb8ea3649226c499
PangPound06 <pondpond6645@gmail.com>
2026-02-21 15:55:32 +0700

    Update Profile

--- src/main/java/com/app/my_project/repository/UserProfileRepository.java
+++ src/main/java/com/app/my_project/repository/UserProfileRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 public interface UserProfileRepository extends JpaRepository<UserProfileEntity, Long> {
     Optional<UserProfileEntity> findByUserId(Long userId);
     boolean existsByUserId(Long userId);
+    Optional<UserProfileEntity> findByEmail(String email);
 }
